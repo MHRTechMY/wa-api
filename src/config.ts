9e87c12d03ca7edf1f6d2394509a1a2874428152
Hafizi Ruslan <pizi@hotmail.my>
2024-09-25 18:04:25 +0800

    rename poweredBy

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export default {
   host: 'http://localhost',
   port: 21465,
   deviceName: 'WPPConnect-Server',
-  poweredBy: 'WPPConnect-Server',
+  poweredBy: '@wppconnect/server',
   startAllSession: true,
   tokenStoreType: 'file',
   maxListeners: 15,
